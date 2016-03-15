f5ea738e058ed48f8935eeb25186fea601ea8060
Juraj Korbacka <jkorbacka@gmail.com>
2016-03-15 14:15:21 +0100

    started work on php

---
+++ templates/header.php
@@ -0,0 +1,22 @@
+<!-- logo + logIn, registrate and basket buttons -->
+<header id="logo">
+    <!-- logo will be set up as backround of header (will be 100% wide and auto height) -->
+    <!--<img src="gallery/logo/logo.png" width="100%">-->
+
+    <aside id="header_logIn_panel">
+        <nav id="login_menu">
+            <ul>
+                <li>
+                    <a href="login.html">LogIn</a>
+                </li>
+                <li>
+                    <a href="register.html">Register</a>
+                </li>
+                <li>
+                    <a id="basket" href="basket.html"></a>
+                </li>
+            </ul>
+
+        </nav>
+    </aside>
+</header>
---
+++ templates/sideNav.php
@@ -0,0 +1,15 @@
+<!-- side bar navigation menu -->
+<aside id="leftSideNavBar">
+    <nav>
+        <ul>
+            <li><a href="basketball.html">Basketball</a></li>
+            <li><a href="tennis.html">Tennis</a></li>
+            <li><a href="bicycle.html">Bicycle</a></li>
+            <li><a href="basketball.html">Basketball</a></li>
+            <li><a href="tennis.html">Tennis</a></li>
+            <li><a href="bicycle.html">Bicycle</a></li>
+            <li><a href="basketball.html">Basketball</a></li>
+            <li><a href="tennis.html">Tennis</a></li>
+        </ul>
+    </nav>
+</aside>
